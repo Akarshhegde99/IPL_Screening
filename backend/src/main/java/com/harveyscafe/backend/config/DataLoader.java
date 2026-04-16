ca83e3255b8e50e221e1e6625899cb416cbc3f31
akbmsit <1by22is012@bmsit.in>
2026-04-16 12:10:28 +0530

    Udpated UI and Workdlow

--- backend/src/main/java/com/harveyscafe/backend/config/DataLoader.java
+++ backend/src/main/java/com/harveyscafe/backend/config/DataLoader.java
@@ -22,48 +22,42 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loading initial match data...");
 
         MatchSession match1 = new MatchSession(
-                null, "CSK", "RCB", 
-                LocalDate.of(2026, 4, 5), LocalTime.of(19, 30), 
+                null, "LSG", "RCB", 
+                LocalDate.of(2026, 4, 15), LocalTime.of(19, 30), 
                 "COMPLETED", "/rcb-poster.jpg"
         );
 
         MatchSession match2 = new MatchSession(
-                null, "RCB", "RR", 
-                LocalDate.of(2026, 4, 10), LocalTime.of(19, 30), 
-                "COMPLETED", "/rcb-poster.jpg"
+                null, "RCB", "DC", 
+                LocalDate.of(2026, 4, 18), LocalTime.of(15, 30), 
+                "UPCOMING", "/rcb-poster.jpg"
         );
 
         MatchSession match3 = new MatchSession(
-                null, "RCB", "MI", 
-                LocalDate.of(2026, 4, 12), LocalTime.of(19, 30), 
-                "COMPLETED", "/rcb-poster.jpg"
+                null, "RCB", "GT", 
+                LocalDate.of(2026, 4, 24), LocalTime.of(19, 30), 
+                "UPCOMING", "/rcb-poster.jpg"
         );
 
         MatchSession match4 = new MatchSession(
-                null, "RCB", "LSG", 
-                LocalDate.of(2026, 4, 15), LocalTime.of(19, 30), 
+                null, "DC", "RCB", 
+                LocalDate.of(2026, 4, 27), LocalTime.of(19, 30), 
                 "UPCOMING", "/rcb-poster.jpg"
         );
 
         MatchSession match5 = new MatchSession(
-                null, "RCB", "KKR", 
-                LocalDate.of(2026, 4, 18), LocalTime.of(19, 30), 
+                null, "GT", "RCB", 
+                LocalDate.of(2026, 4, 30), LocalTime.of(19, 30), 
                 "UPCOMING", "/rcb-poster.jpg"
         );
 
         MatchSession match6 = new MatchSession(
-                null, "RCB", "SRH", 
-                LocalDate.of(2026, 4, 22), LocalTime.of(19, 30), 
+                null, "LSG", "RCB", 
+                LocalDate.of(2026, 5, 7), LocalTime.of(19, 30), 
                 "UPCOMING", "/rcb-poster.jpg"
         );
         
-        MatchSession match7 = new MatchSession(
-                null, "RCB", "PBKS", 
-                LocalDate.of(2026, 4, 25), LocalTime.of(19, 30), 
-                "UPCOMING", "/rcb-poster.jpg"
-        );
-
-        matchRepository.saveAll(Arrays.asList(match1, match2, match3, match4, match5, match6, match7));
+        matchRepository.saveAll(Arrays.asList(match1, match2, match3, match4, match5, match6));
         System.out.println("Initial match data loaded.");
     }
 }
